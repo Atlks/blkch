9a3050f80b1299956c3c9f7ed8152e3fb4dedaae
attilax520 <61309219+attilax520@users.noreply.github.com>
2022-11-25 18:13:18 +0800

    Update fst.js

--- fst.js
+++ fst.js
@@ -2,8 +2,15 @@ console.log(111)
 console.log(222)
 console.log(333)
 
+
+addd prj tmplt
+
+dotnet new console -o MyApp -f net7.0
+
 >dotnet run --project D:\0src
 找不到要运行的项目。请确保 D:\0src 中存在项目，或使用 --project 传递项目路径。
 
 dotnet run --project C:\Users\rr\MyApp
 
+
+bin/xxx.exe file gene..zo

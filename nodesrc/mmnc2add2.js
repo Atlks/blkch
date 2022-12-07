758dd3406d10132ba8ba6cb30373b84eeebdfe60
Your Name <you@example.com>
2022-12-07 14:57:40 +0800

    ..

--- nodesrc/mmnc2add2.js
+++ nodesrc/mmnc2add2.js
@@ -7,14 +7,15 @@ const bip39 = require('bip39')
 const HDWallet = require('ethereum-hdwallet')
 
 //生成助记词
-const mnemonic = "glare pave fatal catch cake large mad exit any hood expose neither"
+const mnemonic = " glare pave fatal catch cake large mad exit any hood expose neither"
 //bip39.generateMnemonic(128,null,bip39.wordlists.chiness_simplified)console.log('助记词' + mnemonic);
 
 //通过随机助记词生成公私钥、地址
 async function getAdress(mnemonic) {
-    const seed = await bip39.mnemonicToSeed(mnemonic)//根据助记词生成seed    
-    const hdwallet = HDWallet.fromSeed(seed)//通过seed获取hdWallet    
-    const key = hdwallet.derive("m/44' /60' /0/0")//源于，得自。设置地址路径    
+    
+    hdwallet= HDWallet.fromMnemonic(mnemonic)
+ //   const hdwallet = HDWallet.fromSeed(seed)//通过seed获取hdWallet    
+    const key = hdwallet.derive("m/44'/60'/0'/0")//源于，得自。设置地址路径    
     console.log("PrivateKey = " + key.getPrivateKey().toString('hex'))//私钥    
     console.log("PublicKey = " + key.getPublicKey().toString('hex'))//公钥    
     const EthAddress = '0x' + key.getAddress().toString('hex')//地址    
@@ -26,7 +27,7 @@ async function getAdressFromSameSeed(mnemonic) {
     const seed = await bip39.mnemonicToSeed(mnemonic)//根据助记词生成seed    
     const hdwallet = HDWallet.fromSeed(seed)//通过seed获取hdWallet  
 
-    for (var i = 0; i < 10; i++) {
+    for (var i = 0; i < 2; i++) {
         const key = hdwallet.derive("m/44' /60' /0/0" + i)//源于，得自。设置地址路径        
         console.log("========地址" + i + "===========")
         console.log("PrivateKey = " + key.getPrivateKey().toString('hex'))//私钥        
@@ -36,7 +37,7 @@ async function getAdressFromSameSeed(mnemonic) {
     }
 }
 getAdress(mnemonic)
-//getAdressFromSameSeed(mnemonic)
+ //getAdressFromSameSeed(mnemonic)
 
 /**
  * 
